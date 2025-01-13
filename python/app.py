83cf11fdd9ddab6ca34fd034b7428264e4467b68
Christopher Francisco <cjfg00gl3@gmail.com>
2025-01-12 23:51:10 -0600

    CREATE app.py to replace main.py and create a flask web server to serve the index / route with a welcome message

---
+++ python/app.py
@@ -0,0 +1,13 @@
+print("WELCOME TO CURSIVE CLASSIFIER!")
+print("I AM GOING TO SLEEP, I OPEN TOMORROW")
+print("DANG, FLASK NOW HAS HOT RELOAD!")
+from flask import Flask
+
+app = Flask(__name__) # our flask app is created
+
+@app.route('/')
+def index():
+    return "<h1>WELCOME TO CURSIVE CLASSIFIER!</h1><br><h2>Designed by Chrissy Franky</h2>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
